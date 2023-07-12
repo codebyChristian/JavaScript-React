16b63c1a42b023546b8e6c25edb62ba35354548c
Christian Santos <christ.cardoso.santos@gmail.com>
2023-07-12 20:40:20 -0300

    Testando criação de card

--- carrinho_react/src/App.js
+++ carrinho_react/src/App.js
@@ -1,23 +1,32 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
 import logo from './logo.svg';
 import './App.css';
+
+
+function ExemploCard() {
+  return (
+    <Card style={{ width: '18rem'}}>
+      <Card.Img variant="top" src="..." />
+      <Card.Body>
+        <Card.Title>Zé da manggaaa</Card.Title>
+        <Card.Text>
+          Hello Mom
+        </Card.Text>
+        <Button variant="primary">Botão</Button>
+      </Card.Body>
+
+    </Card>
+      
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ExemploCard />
     </div>
   );
 }
