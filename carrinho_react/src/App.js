b58f2dd06565ed0d5a989d768b5d0d73f7981822
Christian Santos <christ.cardoso.santos@gmail.com>
2023-07-13 15:25:44 -0300

    Adicionando fotos

--- carrinho_react/src/App.js
+++ carrinho_react/src/App.js
@@ -6,22 +6,7 @@ import logo from './logo.svg';
 import './App.css';
 
 
-function ExemploCard() {
-  return (
-    <Card style={{ width: '18rem'}}>
-      <Card.Img variant="top" src="..." />
-      <Card.Body>
-        <Card.Title>Zé da manggaaa</Card.Title>
-        <Card.Text>
-          Hello Mom
-        </Card.Text>
-        <Button variant="primary">Botão</Button>
-      </Card.Body>
 
-    </Card>
-      
-  )
-}
 
 function App() {
   return (
---
+++ carrinho_react/src/exemplo_card.js
@@ -0,0 +1,18 @@
+
+
+function ExemploCard() {
+    return (
+      <Card style={{ width: '18rem'}}>
+        <Card.Img variant="top" src="..." />
+        <Card.Body>
+          <Card.Title>Zé da manggaaa</Card.Title>
+          <Card.Text>
+            Hello Mom
+          </Card.Text>
+          <Button variant="primary">Botão</Button>
+        </Card.Body>
+  
+      </Card>
+        
+    )
+  }
