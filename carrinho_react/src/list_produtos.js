b3159166c8cbf7b46a6a85430d0edf99af8695ad
Christian Santos <christ.cardoso.santos@gmail.com>
2023-07-13 21:51:55 -0300

    Criando o array para as informações dos produtos

--- carrinho_react/src/list_produtos.js
+++ carrinho_react/src/list_produtos.js
@@ -1,4 +1,9 @@
 import React from "react";
+import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import imagemSabao from './photo_react/sabonete.jpg';
 
 const ListaProduto = () => {
     // Dados do produtos
@@ -8,26 +13,52 @@ const ListaProduto = () => {
             nome: "Sabonete",
             descricao: "Sabonete suave",
             preco: 10.00,
-            imagem: '../photo_react/sabonete'
+            imagem: imagemSabao
         }, 
+        {
+            id: 2,
+            nome: "Brilho",
+            descricao: "Sabonete suave",
+            preco: 10.00,
+            imagem: imagemSabao
+        }, 
+        {
+            id: 3,
+            nome: "Perfume",
+            descricao: "Sabonete suave",
+            preco: 10.00,
+            imagem: imagemSabao
+        },
+        {
+            id: 4,
+            nome: "Batom",
+            descricao: "Sabonete suave",
+            preco: 10.00,
+            imagem: imagemSabao
+        },
         // repita com outros
     ];
 
     return(
         <div>
             <h1>Produtos Disponíveis</h1>
-            <ul>
+            <Row className="row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-0">                
                 {produtos.map(produtos => (
-                    <li key={produtos.id}> 
-                        <img src={produtos.imagem}  alt={produtos.nome} />
-                        <h2>{produtos.name}</h2>
-                        <p>{produtos.descricao}</p>
-                        <p>{produtos.preco}</p>
-                        <p>Preço: R$ {produtos.preco}</p>
 
-                    </li>
+                    <Col key={produtos.id} className="mb-4">
+                        <Card key={produtos.id} style={{width: '100%'}}>
+                            <Card.Img variant="top" src={produtos.imagem} alt={produtos.nome}/>
+                            <Card.Body>
+                                <Card.Title>{produtos.nome}</Card.Title>
+                                <Card.Text>{produtos.descricao}</Card.Text>
+                                <Card.Text>Preço: R$ {produtos.preco}</Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+
                 ))}
-            </ul>
+                
+            </Row>
         </div>
     );
 };
